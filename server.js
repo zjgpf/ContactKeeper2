ffa29ea5b01453299129ba5bb85bceedf0ffd020
PengFei Gao <v_pega@163.com>
2020-09-05 18:08:41 +0800

    prepare for deploy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const connectDB = require('./config/db');
 connectDB();
@@ -12,5 +13,15 @@ app.use(express.json({extended: false}));
 app.use('/api/register', require('./routes/register'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 app.listen( PORT, () => console.log(`Server started on port ${PORT}`));
